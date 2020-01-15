7149b015da0276312329eb6c37c5c81d3699b085
Nyannazz <kantemir.imam@gmail.com>
2020-01-15 21:23:20 +0100

    working for 1 page

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import requests
 import pprint
 from bs4 import BeautifulSoup
 
-""" def getVideos(search)
-    URL="https://www.youtube.com/results?search_query="+search
-    page=requests.get(URL)
-
-    print(page.content) """
 
 URL="https://www.youtube.com/results?search_query=tool"
 page=requests.get(URL).text
@@ -14,8 +9,26 @@ soup=BeautifulSoup(page, "lxml")
 
 results=soup.find_all("a", class_="yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link")
 
-for a in results: 
-    print(a)
-    print("\n")
+videos=soup.find_all("div", class_="yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix")
 
-##print(soup.prettify())
+videoList=[]
+
+#print(videos[0].prettify())
+
+for a in videos: 
+    """ print(a.prettify())
+    print("\n") """
+    listItems=a.ul.find_all("li", recursive=False)
+    thumbnail=a.img.get("data-thumb")
+    thumbnail=thumbnail if thumbnail else a.img.get("src")
+    duration=a.span.text.strip("\n")
+    title=a.h3.a.text
+    videoList.append({"uploaded": listItems[0].text, "views": listItems[1].text, "thumbnail": thumbnail, "duration": duration, "title": title})
+
+""" for item in videoList:
+    print(item)
+    print("\n") """
+
+print(videoList[0])
+
+#print(soup.prettify())
