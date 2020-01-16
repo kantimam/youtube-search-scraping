48c3b207dd1689bcf6bd62be4b158b066e60ed1b
Nyannazz <kantemir.imam@gmail.com>
2020-01-16 14:06:59 +0100

    working for n pages

--- main.py
+++ main.py
@@ -3,32 +3,38 @@ import pprint
 from bs4 import BeautifulSoup
 
 
-URL="https://www.youtube.com/results?search_query=tool"
-page=requests.get(URL).text
-soup=BeautifulSoup(page, "lxml")
+def searchYoutube(query, page=None):
+    pageQuery="&page="+str(page) if page else ""
+    URL="https://www.youtube.com/results?search_query="+query+pageQuery
+    request=requests.get(URL)
+    if(request.status_code==200):
+        page=request.text
+        soup=BeautifulSoup(page, "lxml")
 
-results=soup.find_all("a", class_="yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link")
+        videos=soup.find_all("div", class_="yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix")
+        if(len(videos)):
+            videoList=[]
+            for a in videos: 
+                """ print(a.prettify())
+                print("\n") """
+                listItems=a.ul.find_all("li", recursive=False)
+                thumbnail=a.img.get("data-thumb")
+                thumbnail=thumbnail if thumbnail else a.img.get("src")
+                duration=a.span.text.strip("\n")
+                title=a.h3.a.text
+                videoList.append({"uploaded": listItems[0].text, "views": listItems[1].text, "thumbnail": thumbnail, "duration": duration, "title": title})
 
-videos=soup.find_all("div", class_="yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix")
+            return videoList
+        
+        else:
+            print("could not find video container on page")
+    
+    else: 
+        print("request failed\n")
 
-videoList=[]
-
-#print(videos[0].prettify())
-
-for a in videos: 
-    """ print(a.prettify())
-    print("\n") """
-    listItems=a.ul.find_all("li", recursive=False)
-    thumbnail=a.img.get("data-thumb")
-    thumbnail=thumbnail if thumbnail else a.img.get("src")
-    duration=a.span.text.strip("\n")
-    title=a.h3.a.text
-    videoList.append({"uploaded": listItems[0].text, "views": listItems[1].text, "thumbnail": thumbnail, "duration": duration, "title": title})
-
-""" for item in videoList:
-    print(item)
-    print("\n") """
-
-print(videoList[0])
+#print(searchYoutube("tool", 1))
 
 #print(soup.prettify())
+
+for i in searchYoutube("tool", 1):
+    print(i,"\n")
